0e52036ad28a963aaa92123b7a96cb73d883a2ea
jilani sayyad <jsayyad@slb.com>
2024-01-13 23:51:11 +0530

    updated the nvims

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ return require("packer").startup(function(use)
 	-- Packer
 	use("wbthomason/packer.nvim")
 	-- Colorscheme
-	use({ "ellisonleao/gruvbox.nvim", config = "require('theme-config')" })
+	use({ "xiantang/darcula-dark.nvim", config = "require('theme-config')" })
 	-- Autocomplete
 	use({ "nvim-lua/plenary.nvim" })
 	use({ "hrsh7th/cmp-nvim-lsp" })
