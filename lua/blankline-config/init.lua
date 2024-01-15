afb660e0d53cc43dc9bd15ed1562736cdad6d9cd
jilani sayyad <jsayyad@slb.com>
2024-01-15 19:55:29 +0530

    updated the nvims

--- lua/blankline-config/init.lua
+++ lua/blankline-config/init.lua
@@ -1,2 +1,5 @@
 opts = {}
-require("ibl").setup(require("indent-rainbowline").make_opts(opts))
+
+require("ibl").setup(require("indent-rainbowline").make_opts(opts, {
+  color_transparency = 0.20,
+}))
