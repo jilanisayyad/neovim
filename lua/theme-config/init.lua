c9aa72a27cf7a2a312e4c1c3b08ed31035eb0d34
jilani sayyad <jsayyad@slb.com>
2024-01-21 14:49:28 +0530

    updated the configuration

--- lua/theme-config/init.lua
+++ lua/theme-config/init.lua
@@ -1,6 +1,6 @@
-vim.g.gruvbox_material_background = "medium"
+vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_foreground = "original"
+vim.g.gruvbox_material_foreground = "mix"
 vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_enable_bold = 0
 vim.g.gruvbox_material_cursor = "auto"
